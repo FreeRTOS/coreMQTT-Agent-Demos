2bb69c56a75ddb6f80cb47819564e8b9af3e15fb
RichardBarry <ribarry@amazon.com>
2021-03-25 20:56:34 -0700

    Fix dependency generation in GCC/QEMU makefile.

--- build/Cortex-M3_MPS2_QEMU_GCC/FreeRTOSConfig.h
+++ build/Cortex-M3_MPS2_QEMU_GCC/FreeRTOSConfig.h
@@ -49,8 +49,8 @@
 #define configUSE_DAEMON_TASK_STARTUP_HOOK       0
 #define configCPU_CLOCK_HZ                       ( ( unsigned long ) 20000000 )
 #define configTICK_RATE_HZ                       ( ( TickType_t ) 1000 )
-#define configMINIMAL_STACK_SIZE                 ( ( unsigned short ) 1000 )//_RB_
-#define configTOTAL_HEAP_SIZE                    ( ( size_t ) ( 80000 ) )
+#define configMINIMAL_STACK_SIZE                 ( ( unsigned short ) 500 )
+#define configTOTAL_HEAP_SIZE                    ( ( size_t ) ( 120000 ) )
 #define configMAX_TASK_NAME_LEN                  ( 10 )
 #define configUSE_TRACE_FACILITY                 1
 #define configUSE_16_BIT_TICKS                   0
