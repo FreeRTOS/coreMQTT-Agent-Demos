125e2714bbab42a80c372840d248844095acc80a
RichardBarry <ribarry@amazon.com>
2021-03-29 08:47:56 -0700

    Make the FreeRTOS kernel makefile more generic - so you only need to change one constant to switch ports.

--- build/Cortex-M3_MPS2_QEMU_GCC/freertos-kernel.mk
+++ build/Cortex-M3_MPS2_QEMU_GCC/freertos-kernel.mk
@@ -2,9 +2,10 @@
 #port.
 
 KERNEL_DIR += ./../../lib/FreeRTOS/freertos-kernel
+KERNEL_PORT_DIR += $(KERNEL_DIR)/portable/GCC/ARM_CM3
 INCLUDE_DIRS += -I$(KERNEL_DIR)/include \
-				-I$(KERNEL_DIR)/portable/GCC/ARM_CM3
-VPATH += $(KERNEL_DIR) $(KERNEL_DIR)/portable/GCC/ARM_CM3 $(KERNEL_DIR)/portable/MemMang
+				-I$(KERNEL_PORT_DIR)
+VPATH += $(KERNEL_DIR) $(KERNEL_PORT_DIR) $(KERNEL_DIR)/portable/MemMang
 SOURCE_FILES += $(KERNEL_DIR)/tasks.c
 SOURCE_FILES += $(KERNEL_DIR)/list.c
 SOURCE_FILES += $(KERNEL_DIR)/queue.c
